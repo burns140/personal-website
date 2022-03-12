099fd3e0b57a0648ac03a99af36587c463be53a0
Stephen Burns <burns140@purdue.edu>
2022-03-11 21:46:03 -0700

    change to use sendfile instead of res.send(data)

--- server/routes/aboutMe.js
+++ server/routes/aboutMe.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile("./misc/about-me.txt");
+    fs.readFile('./misc/about-me.txt', (err, data) => {
+        res.set("Content-Type", "text/plain");
+        res.send(data);
+    });
 })
 
 module.exports = router;
